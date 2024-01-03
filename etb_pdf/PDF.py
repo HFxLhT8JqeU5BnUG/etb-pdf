945d80ab0ff0669fc4466acbd276518e3f8b968f
tate <yg3bpwn0Or679Hau8Fxi@duck.com>
2024-01-03 08:49:42 -0600

    minor changes

--- etb_pdf/PDF.py
+++ etb_pdf/PDF.py
@@ -7,7 +7,6 @@ class Pdf:
 
     def __init__(self, template_file_path: str, output_dir: str = None):
         self.reader = PdfReader(template_file_path)
-        # print(self.reader.get_form_text_fields())
 
         if output_dir is not None:
             self.output_dir = output_dir
@@ -15,13 +14,14 @@ class Pdf:
             self.output_dir = ''
 
 
-    def write(self, rows: list[dict[str, Any]] = None, map: dict[str, Any] = None, 
+    def write(self, rows: list[dict[str, Any]] = None, map: dict[str, str] = None, 
               naming: dict[str, str] = None):
         '''check the README for more info'''
 
         if map is not None:
             assert all(value in row for value in map.values() for row in rows)
-            data = [{key : row[value] for key, value in map.items()} for row in rows]
+            data = [{key : row[value] for key, value in map.items()}
+                    for row in rows]
 
         else:
             data = rows
@@ -30,7 +30,6 @@ class Pdf:
         dynamic_name_key = naming.get('dynamic_name_key', '')
 
         for row in data:
-            # print(row)
             writer = PdfWriter()
             page = self.reader.pages[0]
 
@@ -41,7 +40,17 @@ class Pdf:
             )
 
             output_file_name = f"{self.output_dir}{static_name}_{row.get(dynamic_name_key, '')}.pdf"
-            # print(output_file_name)
 
             with open(output_file_name, "wb") as output_stream:
                 writer.write(output_stream)
+
+
+    def get_fields(self, text_only: bool = False) -> dict[str, Any]:
+        '''get PDF fields (all or text fields only)'''
+
+        if text_only:
+            fields = self.reader.get_form_text_fields()
+        else:
+            fields = self.reader.get_fields()
+
+        return fields
